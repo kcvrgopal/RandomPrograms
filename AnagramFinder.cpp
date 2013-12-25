3630daf5450232f6e3f6eab961cedd6f883c0ef4
Rajagopalan Venkataramanan <rajagopalan@hotmail.com>
2013-12-24 16:48:22 -0800

    AnagramFinder Logic Enhanced

--- AnagramFinder.cpp
+++ AnagramFinder.cpp
@@ -1,31 +1,91 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
-int main()
+bool compare(string s1, string s2)
 {
-	string s1,s2;
 	int c=0;
-	cout<<"Enter the First Word"<<endl;
-	cin>>s1;
-	cout<<"Enter the Second word"<<endl;
-	cin>>s2;
-	cout<<"Processing..."<<endl;
 	for(int i=0;i<s1.size();i++)
 	{
-		for(int j=0;j<s2.size();j++)
+		for(int j=0;j<s1.size();j++)
 		{
 			if(s1[i]==s2[j])
 			{
 				c++;
+				break;
 			}
 		}
 	}
-	if(c!=s1.size())
+	if(c==s1.size())
 	{
-		cout<<s1<<" and "<<s2<<" are not Anagrams"<<endl;
+		return true;
 	}
 	else
 	{
+		return false;
+	}
+}
+void findAnagram(string s1, string s2)
+{
+	int c=0;
+	int pos=0;
+	char *p;
+	int s=s1.size();
+	char temp[s];	
+	for(int i=0;i<s1.size();i++)
+	{
+		for(int j=0;j<s2.size();j++)
+		{
+			
+			if(s1[i]==s2[j])
+			{
+				if(pos>j)
+				{
+					//skip=false;
+					continue;
+					
+				}
+				else
+				{
+					//cout<<i<<endl;
+					for(int k=0;k<s;k++)
+					{
+						if(strrchr(temp,s2[j])>0)
+						{
+							p=strrchr(temp,s2[j]);
+							pos=p-temp;
+						}
+					}
+					temp[i]=s2[j];
+					break;
+				}
+			}
+		}
+	}
+	//cout<<temp<<endl;
+	//cout<<strlen(temp);
+	if(compare(s1,temp))
+	{
 		cout<<s1<<" and "<<s2<<" are Anagrams"<<endl;
 	}
+	else
+	{
+		cout<<s1<<" and "<<s2<<" are not Anagrams"<<endl;
+	}
+}
+int main()
+{
+	string s1,s2;
+	char ch;
+	do
+	{
+		cout<<"Enter the First Word"<<endl;
+		cin>>s1;
+		cout<<"Enter the Second word"<<endl;
+		cin>>s2;
+		cout<<"Processing..."<<endl;
+		findAnagram(s1,s2);
+		cout<<"Want to try again? Y or N"<<endl;
+		cin>>ch;
+	}while(ch=='Y'||ch=='y');
 	return 0;
 }
