bf1797f217696160759eaf4d5c7c3ee98b1abcd9
Rajagopalan Venkataramanan <rajagopalan@hotmail.com>
2013-12-25 14:57:56 -0800

    DuplicateElement Finder updated

---
+++ DuplicateElementFinder.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int* generateRandom(int n)
+{
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=i+1;
+	}
+	int j=rand() % n;
+	int v=(rand() % n) + 1;
+	a[j]=v;
+	return a;
+	
+}
+int check(int a[],int n)
+{
+	for(int i=0;i<n-2;i++)
+	{
+		for(int j=i+1;j<n-1;j++)
+		{
+			if(a[j]==a[i])
+			{
+				cout<<"\n"<<a[j]<<" is a duplicate at index : "<<i<<" and "<<j<<"\n";
+			}
+		}
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Number of elements in an array?"<<endl;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=i+1;
+	}
+	int j=rand() % 100;
+	int v=(rand() % 100) + 1;
+	a[j]=v;
+	check(a,n);
+	return 0;
+}
